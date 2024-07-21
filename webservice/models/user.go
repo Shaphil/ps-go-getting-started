14eb21c91ee5d22b6554ac2af38f652e219aade9
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-07-21 20:28:49 +0600

    Add Module 06 - Creating Functions and Methods > Lesson 05 - Using Methods to Add Behaviors to a Type

--- webservice/models/user.go
+++ webservice/models/user.go
@@ -11,3 +11,15 @@ var (
 	// nextID int = 1
 	nextID = 1 // the same as the above line
 )
+
+func GetUsers() []*User {
+	return users
+}
+
+func AddUser(user User) (User, error) {
+	user.ID = nextID
+	nextID++
+	users = append(users, &user)
+
+	return user, nil
+}
